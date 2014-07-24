c6e8c0b310fe4ed975fbf15a9f44d1b5133a0298
HasibShakur <hasib.shakur@gmail.com>
2014-07-23 20:06:31 -0500

    changed d_expr.toString() to d_expr

--- src/expr/command.cpp
+++ src/expr/command.cpp
@@ -205,7 +205,7 @@ void QESimplifyCommand::invoke(SmtEngine* smtEngine) throw() {
 }
 void QESimplifyCommand::invoke(SmtEngine* smtEngine, std::ostream& out) throw() {
  // CVC4::Expr ex = smtEngine->eliminateQuantifier(d_expr);
-  out << d_expr.toString() << std::endl;
+  out << d_expr << std::endl;
   d_commandStatus = CommandSuccess::instance();
   printResult(out, smtEngine->getOption("command-verbosity:" + getCommandName()).getIntegerValue().toUnsignedInt());
 }
