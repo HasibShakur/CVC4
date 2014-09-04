da3cb0319886f3bf729ae85ee7a5705ddef27b65
HasibShakur <hasib.shakur@gmail.com>
2014-09-04 06:41:36 -0500

    created a constant from a node

--- src/util/QuantifierEliminate.cpp
+++ src/util/QuantifierEliminate.cpp
@@ -271,7 +271,8 @@ Node QuantifierEliminate::internalProcessNodeQE(Node n)
     Constant result = Constant::mkConstant(c.getValue()+one.getValue());
     return result.getNode();*/
     Debug("expr-qetest")<<"Constant "<<n.getType()<<"\n";
-    Debug("expr-qetest")<<"Constant value "<<n.getConst<Rational><<"\n";
+    Constant c = Constant::mkConstant(n);
+    Debug("expr-qetest")<<"Constant value "<<c.getValue()<<"\n";
 
    // Debug("expr-qetest")<<"Constant "<<n.getType(true).getConst()<<"\n";
     return n;
