bc48ac31a4c4e2afd86f1bca7e878b0b16cb595e
MDHASIBBIN <MDHASIBBIN@HasibShakur>
2014-10-18 03:24:46 -0500

    added debug message

--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -3353,6 +3353,7 @@ std::string SmtEngine::eliminateQuantifier(Expr ex) {
   SmtScope smts(this);
   Debug("expr-qetest")<<"Before processing "<<ex<<"\n";
   Node t1 = QuantifierEliminate::preRewriteForPrenex(NodeTemplate<true>(ex));
+  Debug("expr-qetest")<<"After pre rewriting processing "<<t1<<"\n";
   Node t2 = QuantifierEliminate::postRewriteForPrenex(t1);
 //  Node processedNode = QuantifierEliminate::doPreprocessing(ex);
   Debug("expr-qetest")<<"After processing "<<t2<<"\n";
