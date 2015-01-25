c1110256fc2639dd3a2c73133c2a197aa261ff5f
MDHASIBBIN <MDHASIBBIN@HasibShakur>
2015-01-25 17:37:31 -0600

    resolved error in multiple bound var method

--- src/expr/QuantifierEliminate.cpp
+++ src/expr/QuantifierEliminate.cpp
@@ -2512,11 +2512,11 @@ Node QuantifierEliminate::performCaseAnalysis(Node n, std::vector<Node> bv) {
 
 std::vector<Node> QuantifierEliminate::computeMultipleBoundVariables(Node n) {
   std::vector<Node> multipleBoundVars;
-  Debug("expr-qetest")<<"n[0] "<<n[0]<<std::endl;
-  if(n[0].getNumChildren() > 1) {
+  Debug("expr-qetest")<<"n "<<n<<std::endl;
+  if(n.getNumChildren() > 1) {
     for(int i = 0; i < (int) n.getNumChildren(); i++) {
-      Debug("expr-qetest")<<"boundVar "<<n[0][i]<<std::endl;
-      multipleBoundVars.push_back(n[0][i]);
+      Debug("expr-qetest")<<"boundVar "<<n[i]<<std::endl;
+      multipleBoundVars.push_back(n[i]);
     }
   }
   else
