6521aa1ff33b80444142c165e14b533fed45b663
MDHASIBBIN <MDHASIBBIN@HasibShakur>
2015-01-26 00:12:44 -0600

    modified projections

--- src/expr/QuantifierEliminate.cpp
+++ src/expr/QuantifierEliminate.cpp
@@ -2637,7 +2637,7 @@ Node QuantifierEliminate::computeProjections(Node n) {
     if(temp.getKind() == kind::FORALL) {
       std::vector < Node > bv = computeMultipleBoundVariables(temp[0]);
       boundVar.push_back(bv);
-      args.push_back(temp[1]);
+      args.push_back(temp[1].negate());
       return computeProjections(temp[1]);
     } else if(temp.getKind() == kind::AND) {
       std::vector<Node> miniscopedNode;
