012732232b50c172c205ae6ab48af7b9a2917d1c
MDHASIBBIN <MDHASIBBIN@HasibShakur>
2015-01-30 20:06:08 -0600

    added debug message

--- src/expr/QuantifierEliminate.cpp
+++ src/expr/QuantifierEliminate.cpp
@@ -3556,7 +3556,7 @@ Node QuantifierEliminate::boundVarTypeChecker(Node n) {
           else
           {
             Debug("expr-qetest")<<"exp in not "<<c<<std::endl;
-            boundVarTypeChecker(c);
+            toReturn = boundVarTypeChecker(c);
           }
         }
       }
@@ -3643,7 +3643,7 @@ Node QuantifierEliminate::boundVarTypeChecker(Node n) {
         else
         {
           Debug("expr-qetest")<<"exp "<<c1<<std::endl;
-          boundVarTypeChecker(c1);
+          toReturn = boundVarTypeChecker(c1);
         }
       }
     }
