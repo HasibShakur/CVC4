b75e4ed9d1b558ecea89208725a7ae80aad5ed9c
MDHASIBBIN <MDHASIBBIN@HasibShakur>
2015-03-12 17:00:17 -0500

    changed system test file

--- test/system/two_smt_lia_test.cpp
+++ test/system/two_smt_lia_test.cpp
@@ -33,10 +33,11 @@ int main() {
   smt2.setOption("produce-models", true);
   Expr y1 = em.mkVar("y",integer);
   Expr x1 = em.mkVar("x",integer);
-  Expr ex1 = em.mkExpr(kind::EQUAL,x1,smt.getValue(x));
-  Expr ex2 = em.mkExpr(kind::EQUAL,y1,smt.getValue(y));
+  Expr ex1 = em.mkExpr(kind::PLUS,x1,smt.getValue(x));
+  Expr ex2 = em.mkExpr(kind::PLUS,y1,smt.getValue(y));
   Expr ex3 = em.mkExpr(kind::AND,ex1,ex2);
-  Expr final = em.mkExpr(kind::EQUAL,ex3,exp1);
+  Expr ex4 = em.mkExpr(kind::EQUA,ex2,em.mkConst(Rational(0)));
+  Expr final = em.mkExpr(kind::EQUAL,ex4,exp1);
   Result r2 = smt2.query(final);
   std::cout<<"final "<<r2.toString()<<std::endl;
 
